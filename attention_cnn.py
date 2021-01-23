26ba86a6c1579771cb05e8498378fa79186c43ce
Ugenteraan <m.ugenteraan15@gmail.com>
2021-01-23 12:14:04 +0800

    bug model filter size

--- attention_cnn.py
+++ attention_cnn.py
@@ -48,7 +48,7 @@ class AttentionCNN(nn.Module):
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=128, out_channels=40, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True)
         )
 
